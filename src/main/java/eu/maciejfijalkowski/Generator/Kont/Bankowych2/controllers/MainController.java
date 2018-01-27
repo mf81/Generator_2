07899eaa0a0b6d7a904e8133703cdb4e9e814e13
mf81 <maciej.fijalkowski81@gmail.com>
2018-01-27 13:07:01 +0100

    add validations

--- src/main/java/eu/maciejfijalkowski/Generator/Kont/Bankowych2/controllers/MainController.java
+++ src/main/java/eu/maciejfijalkowski/Generator/Kont/Bankowych2/controllers/MainController.java
@@ -6,25 +6,47 @@ import eu.maciejfijalkowski.Generator.Kont.Bankowych2.models.services.BankDropLi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class MainController {
+
+    private FinalBankAcount finalBankAcount;
+
     @Autowired
     BankDropListCollection bankDropListCollection;
 
     @GetMapping("/")
     public String index (ModelMap modelMap, BankDropListCollection bankDropListCollection){
+        finalBankAcount = new FinalBankAcount();
+        modelMap.addAttribute("finalBankAcount",finalBankAcount);
         modelMap.addAttribute("bankDropListCollection",bankDropListCollection.getBankList());
+
+        System.out.println("1"+finalBankAcount);
         return "index";
     }
 
+    @GetMapping
+    public String generate(ModelMap modelMap){
+        return "generate";
+    }
+
     @PostMapping("/generate")
-    public String generate(ModelMap modelMap, FinalBankAcount finalBankAcount){
+    public String generate(@ModelAttribute @Valid FinalBankAcount finalBankAcount, BindingResult bindingResult, ModelMap modelMap){
+
+//        modelMap.addAttribute("info", "Poprawnie dodałeś/aś nowe ostrzeżenie!");
+
+
         //modelMap.addAttribute("admDropListCollection",admDropListCollection.getAdmy());
         modelMap.addAttribute("admDropListCollection", CsvReader.CsvReader("src/main/resources/adm.csv").entrySet());
         modelMap.addAttribute("acountDropListCollection",CsvReader.CsvReader("src/main/resources/type.csv").entrySet());
+
+        System.out.println("Bank:"+finalBankAcount.getBankNumber());
         if (finalBankAcount.getBankNumber().equals("1320 0022"))
             modelMap.addAttribute("swrkDropListCollection",CsvReader.CsvReader("src/main/resources/swrkPocztowy.csv").entrySet());
         else
@@ -33,13 +55,28 @@ public class MainController {
             else
                 modelMap.addAttribute("swrkDropListCollection",CsvReader.CsvReader("src/main/resources/swrkNord.csv").entrySet());
 
-        modelMap.addAttribute("finalBankAcount",finalBankAcount);
+//        modelMap.addAttribute("finalBankAcount",finalBankAcount);
         System.out.println("Obiekt " + finalBankAcount.getName()+finalBankAcount.getBankNumber());
+        System.out.println(finalBankAcount);
         return "generate";
     }
 
     @PostMapping("/finalresault")
-    public String finalGenerate(ModelMap modelMap, FinalBankAcount finalBankAcount){
+    public String finalGenerate(@ModelAttribute @Valid FinalBankAcount finalBankAcount, BindingResult bindingResult, ModelMap modelMap){
+        if (bindingResult.hasErrors()){
+            modelMap.addAttribute("admDropListCollection", CsvReader.CsvReader("src/main/resources/adm.csv").entrySet());
+            modelMap.addAttribute("acountDropListCollection",CsvReader.CsvReader("src/main/resources/type.csv").entrySet());
+
+            System.out.println("Bank:"+finalBankAcount.getBankNumber());
+            if (finalBankAcount.getBankNumber().equals("1320 0022"))
+                modelMap.addAttribute("swrkDropListCollection",CsvReader.CsvReader("src/main/resources/swrkPocztowy.csv").entrySet());
+            else
+            if (finalBankAcount.getBankNumber().equals("1020 4274"))
+                modelMap.addAttribute("swrkDropListCollection",CsvReader.CsvReader("src/main/resources/swrkPKO.csv").entrySet());
+            else
+                modelMap.addAttribute("swrkDropListCollection",CsvReader.CsvReader("src/main/resources/swrkNord.csv").entrySet());
+            return "generate";
+        }
         modelMap.addAttribute("finalBankAcount",finalBankAcount);
         finalBankAcount.setNRB();
         System.out.println("Obiekt " + finalBankAcount.getName());
--- src/main/java/eu/maciejfijalkowski/Generator/Kont/Bankowych2/models/FinalBankAcount.java
+++ src/main/java/eu/maciejfijalkowski/Generator/Kont/Bankowych2/models/FinalBankAcount.java
@@ -1,5 +1,7 @@
 package eu.maciejfijalkowski.Generator.Kont.Bankowych2.models;
 
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.math.BigInteger;
 
 public class FinalBankAcount {
@@ -9,17 +11,20 @@ public class FinalBankAcount {
     private String swrk;
     private String numberEF;
     private String admNumber;
+    @Pattern(regexp = "^\\d{4}$", message = "Wpisz 4 cyfry dla kodu WM")
     private String WMname;
+    @Pattern(regexp = "^\\d{4}$", message = "Wpisz 4 cyfry dla kodu kontrahenta")
     private String clientName;
 
-    public FinalBankAcount(String name, String bankNumber, String swrk, String numberEF, String admNumber, String WMname, String clientName) {
-        this.name = name;
-        this.bankNumber = bankNumber;
-        this.swrk = swrk;
-        this.numberEF = numberEF;
-        this.admNumber = admNumber;
-        this.WMname = WMname;
-        this.clientName = clientName;
+    public FinalBankAcount(){
+            //String name, String bankNumber, String swrk, String numberEF, String admNumber, String WMname, String clientName) {
+//        this.name = name;
+//        this.bankNumber = bankNumber;
+//        this.swrk = swrk;
+//        this.numberEF = numberEF;
+//        this.admNumber = admNumber;
+//        this.WMname = WMname;
+//        this.clientName = clientName;
     }
 
     public String getCheckSum() {
@@ -43,10 +48,10 @@ public class FinalBankAcount {
     }
 
     public void setBankNumber(String bankNumber) {
-        if (this.bankNumber.equals("1320 0022"))this.name="Pocztowy";
-        if (this.bankNumber.equals("1020 4274"))this.name="PKO BP";
-        if (this.bankNumber.equals("1370 1398"))this.name="Nord Bank";
-        //this.bankNumber = bankNumber;
+        if (bankNumber.equals("1320 0022"))this.name="Pocztowy";
+        if (bankNumber.equals("1020 4274"))this.name="PKO BP";
+        if (bankNumber.equals("1370 1398"))this.name="Nord Bank";
+        this.bankNumber = bankNumber;
     }
 
     public String getSwrk() {
@@ -93,16 +98,16 @@ public class FinalBankAcount {
         this.checkSum = nrbGenerator(bankNumber+swrk+numberEF+admNumber+WMname+clientName);
     }
 
-    public static String nrbGenerator (String nr){
+    private static String nrbGenerator (String nr){
         nr=nr.replaceAll("\\s+","");
-        BigInteger nrKonta, modulo, wynik,liczba;
-        nrKonta = new BigInteger(nr+"252100");
+        BigInteger nrAcount, modulo, subFinal,number;
+        nrAcount = new BigInteger(nr+"252100");
         modulo = new BigInteger("97");
-        liczba = new BigInteger("98");
-        wynik = nrKonta.mod(modulo);
-        wynik=liczba.subtract(wynik);
-        String y = ""+wynik;
-        if (y.length()==1)y="0" + y;
-        return y;
+        number = new BigInteger("98");
+        subFinal = nrAcount.mod(modulo);
+        subFinal=number.subtract(subFinal);
+        String finalNumber = ""+subFinal;
+        if (finalNumber.length()==1)finalNumber="0" + finalNumber;
+        return finalNumber;
     }
 }
